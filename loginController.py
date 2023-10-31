2d349184f8aaa0b883af869a7fa0f8aab7d78683
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2023-10-31 15:49:05 -0300

    Update loginController.py

--- loginController.py
+++ loginController.py
@@ -1,10 +1,10 @@
 #Só lembrando que esse código vai ser mudado devido que não é uma classe e também não temos nossa base de dados
 import psycopg2 as psy
 #Definindo os variaveis de entrada
-idDigitado = "0150501"
+idDigitado = "01505010"
 senhaDigitada = "Rua Anita Ferraz"
 #Conectando a database
-conn = psy.connect( dbname="Vendas", user="postgres", password="Koi.io") #teste
+conn = psy.connect( dbname="Vendas", user="postgres", password="Koi.io")
 #Criando o cursor para poder fazer operações sql
 cur = conn.cursor()
 cur.execute("SELECT cep.cepid, cep.logradouro FROM cep WHERE cepid = %s and logradouro = %s", (idDigitado, senhaDigitada))
@@ -13,3 +13,6 @@ resultado = cur.fetchall()
 #Printando o resultado
 if len(resultado) == 0:
     print("Resultado nao encontrado")
+else:
+    print(resultado)
+
