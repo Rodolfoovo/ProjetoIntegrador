f9e4771827cbb9d3a5f83abc12cc3628d86b9dd0
Rodolfoovo <dansouza359157@gmail.com>
2023-11-02 14:25:55 -0300

    Modelei a classe

--- loginController.py
+++ loginController.py
@@ -1,21 +1,26 @@
 #Só lembrando que esse código vai ser mudado devido que não é uma classe e também não temos nossa base de dados
 import psycopg2 as psy
+class DadosLogin:
 #Definindo os variaveis de entrada
-idDigitado = "01505010"
-senhaDigitada = "Rua Anita Ferraz"
-#Conectando a database
-conn = psy.connect( dbname="Vendas", user="postgres", password="Koi.io")
-#Criando o cursor para poder fazer operações sql
-cur = conn.cursor()
-cur.execute("SELECT cep.cepid, cep.logradouro FROM cep WHERE cepid = %s and logradouro = %s", (idDigitado, senhaDigitada))
-#Adicionando o resultado da consulta em uma variavel
-resultado = cur.fetchall()
-#Printando o resultado
-if len(resultado) == 0:
-    print("Resultado nao encontrado")
-else:
-    print(resultado)
-
+    def __init__(self,loginId,senha):
+        self.loginId = "01505010"
+        self.senha = "Rua Anita Ferraz"
+    def verificaNivelDeAcesso(self):
+        #Conectando a database
+        conn = psy.connect( dbname="Vendas", user="postgres", password="Koi.io")
+        #Criando o cursor para poder fazer operações sql
+        cur = conn.cursor()
+        cur.execute("SELECT cep.cepid, cep.logradouro FROM cep WHERE cepid = %s and logradouro = %s", (self.loginId, self.senha))
+        #Adicionando o resultado da consulta em uma variavel
+        resultado = cur.fetchall()
+        #Printando o resultado
+        if len(resultado) == 0:
+            print("Resultado nao encontrado")
+        else:
+            print(resultado)
+if __name__ == "__main__":
+    dados1 = DadosLogin("01505010","Rua Anita Ferraz")
+    dados1.verificaNivelDeAcesso()
 #TESTE, DANIEL VAGABUNDO
 #mere rme ksa
 #RODOLFREDO É DEUS
