97bc9f7f3d0681f3988de68e81b58d190caa10fc
odrerir <victorcaua03@gmail.com>
2023-11-06 13:05:58 -0300

    feito teste de filtros

--- dash/Telas.py
+++ dash/Telas.py
@@ -1,69 +1,78 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output
 import pandas as pd
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
-    
-app = Dash(__name__, external_stylesheets=external_stylesheets),
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+def generate_table(dataframe, columns_to_show, max_rows=10):
+    # Filtra o DataFrame para mostrar apenas as colunas selecionadas
+    filtered_df = dataframe[columns_to_show]
+    return html.Div([
+        html.Table([
+            html.Thead(
+                html.Tr([html.Th(col) for col in filtered_df.columns])
+            ),
+            html.Tbody([
+                html.Tr([
+                    html.Td(filtered_df.iloc[i][col]) for col in filtered_df.columns
+                ]) for i in range(min(len(filtered_df), max_rows))
+            ])
         ])
     ])
+
+def generate_filter_column(df):
+    return html.Div([
+        html.H3('Filtros'),
+        dcc.Checklist(
+            id='column-selector',
+            options=[{'label': col, 'value': col} for col in df.columns],
+            value=df.columns[:3]  # Colunas selecionadas por padrão
+        )
+    ], style={'float': 'left', 'width': '20%'})
 
 app.layout = html.Div([
     dcc.Tabs(id="tabs", value='tab-1', children=[
-        dcc.Tab(label='DashBoard', value='tab-1'),
-        dcc.Tab(label='Relatorio', value='tab-2'),
-        dcc.Tab(label='Rank Produtos', value='tab-3'),
-        dcc.Tab(label='Entrada e Saida de estoque', value='tab-4'),
+        dcc.Tab(label='Painel de Controle', value='tab-1'),
+        dcc.Tab(label='Relatório', value='tab-2'),
+        dcc.Tab(label='Ranking de Produtos', value='tab-3'),
+        dcc.Tab(label='Entrada e Saída de Estoque', value='tab-4'),
         dcc.Tab(label='Produtos', value='tab-5'),
-        dcc.Tab(label='Gerenciar Usuarios', value='tab-6'),
+        dcc.Tab(label='Funcionários', value='tab-6'),
     ]),
     html.Div(id='tabs-content'),
-    html.Div([
-        generate_table(df),
-    ])
 ])
 
-@callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
+@app.callback(Output('tabs-content', 'children'), [Input('tabs', 'value')])
 def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
-            html.H3('DashBoard'),
+            html.H3('Painel de Controle'),
         ])
     elif tab == 'tab-2':
         return html.Div([
-            html.H3('Relatorios'),
+            html.H3('Relatório'),
         ])
     elif tab == 'tab-3':
         return html.Div([
-            html.H3('Relatorios'),
+            html.H3('Ranking de Produtos'),
+            generate_filter_column(df),
         ])
     elif tab == 'tab-4':
         return html.Div([
-            html.H3('Relatorios'),
+            html.H3('Entrada e Saída de Estoque'),
+            generate_filter_column(df),
         ])
     elif tab == 'tab-5':
         return html.Div([
-            html.H3('Relatorios'),
+            html.H3('Produtos'),
+            generate_filter_column(df),
         ])
     elif tab == 'tab-6':
-        return html.Div([
-            html.H3('Relatorios'),
-        ])
-    elif tab == 'tab-7':
-        return html.Div([
-        ])
-
-
+        return generate_table(df, df.columns[:3])
+    else:
+        return html.Div([])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
