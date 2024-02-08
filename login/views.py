fda0c9337d4caab1b76048724ec86004f1e04416
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-07 21:41:30 -0300

    tentando implementar session

--- login/views.py
+++ login/views.py
@@ -5,15 +5,25 @@ import logging
 from CRUDfuncionario.models import Funcionario
 logger = logging.getLogger(__name__)
 
+
 def login_view(request):
     if request.method == "GET":
         return render(request,'login.html')
     else:
-        idFuncionario = request.POST.get("idFuncionario") 
+        username = request.POST.get("username") 
         password = request.POST.get("password")
 
-        user = authenticate(request,username=idFuncionario,password=password)
+        user = authenticate(request,username=username,password=password)
         if user:
+#            context = {
+#                'username': user.username,
+#                'enderecoFuncionario': user.enderecoFuncionario,
+#                'cpf':user.cpf,
+#                'cep':user.cep,
+#                'telefone':user.telefone,
+#                'funcao':user.funcao,
+#                'email':user.email
+#            }
             return redirect('telainicial')
         else:
             return HttpResponse('Email ou senha invalidos')
