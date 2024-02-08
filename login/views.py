742cd2b12515328715926669266da1e874154af7
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-08 10:42:37 -0300

    testando se as sessions funcionam na aplicação

--- login/views.py
+++ login/views.py
@@ -15,15 +15,17 @@ def login_view(request):
 
         user = authenticate(request,username=username,password=password)
         if user:
-#            context = {
-#                'username': user.username,
-#                'enderecoFuncionario': user.enderecoFuncionario,
-#                'cpf':user.cpf,
-#                'cep':user.cep,
-#                'telefone':user.telefone,
-#                'funcao':user.funcao,
-#                'email':user.email
-#            }
+            userData = {
+                'username': user.username,
+                'enderecoFuncionario': user.enderecoFuncionario,
+                'cpf':user.cpf,
+                'CEP':user.CEP,
+                'telefone':user.telefone,
+                'funcao':user.funcao,
+                'email':user.email
+            }
+            request.session['userData'] = userData
+#            return redirect('logout')
             return redirect('telainicial')
         else:
             return HttpResponse('Email ou senha invalidos')
@@ -31,3 +33,6 @@ def login_view(request):
 def reset_senha_view(request):
     # Lógica para a página de redefinição de senha
     return render(request, 'Resetarsenha.html')
+def logout_view(request):
+    userData = request.session['userData']
+    return render(request, 'logout.html',{'userData':userData})
