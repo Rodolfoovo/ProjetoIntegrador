2a674b3c5544ad6aabae9b1ca5cd7f6ea973719b
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-20 15:29:30 -0300

    Possibilitando que o botão perfil leve ao logout

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import tela_inicial
+from login.views import logout_view
 urlpatterns = [
     path('', tela_inicial, name='telainicial'),
+    path('logout/',logout_view,name='logout' )
 ]
