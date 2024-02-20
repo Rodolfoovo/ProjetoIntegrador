4d8e4d17edc2a337ddcc9231588bcfb5fa1c8e36
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-20 15:44:50 -0300

    Gerenciar usuário agora acessa o CRUD do funcionario

--- Produtos/models.py
+++ Produtos/models.py
@@ -11,3 +11,11 @@ from django.db import models
 #categoria = models.CharField(max_length=15)
 #    subCategoria=models.CharField(max_length=15)
 #    marcaProduto=models.CharField(max_length=15)
+
+class Fornecedor(models.Model):
+    idFornecedor = models.AutoField(primary_key=True)
+    nomeFornecedor = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=30)
+    telefone = models.CharField(max_length=20)
+    cep = models.CharField(max_length=8)
+    cnpj = models.CharField(max_length=14)
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import tela_inicial
 from login.views import logout_view
+from CRUDfuncionario.views import home
 urlpatterns = [
     path('', tela_inicial, name='telainicial'),
-    path('logout/',logout_view,name='logout' )
+    path('logout/',logout_view,name='logout' ),
+    path('home/', home , name='home')
 ]
