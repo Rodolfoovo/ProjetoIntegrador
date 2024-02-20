09777bf20413f9f5ef6fbf86d7fd94bfa61f31bf
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-20 16:09:13 -0300

    criando fornecedor, ainda não está na base de dados

--- Produtos/models.py
+++ Produtos/models.py
@@ -5,12 +5,13 @@ from django.db import models
 
 #class Produtos(models.Model):
 #   idProduto = models.AutoField(primary_key=True)
-#  nomeProduto=models.CharField(max_length= 30)
-# valorUnit= models.DecimalField(max_digits=10,decimal_places=2)
-#qntEstoque =models.IntegerField(default = 1)
-#categoria = models.CharField(max_length=15)
-#    subCategoria=models.CharField(max_length=15)
-#    marcaProduto=models.CharField(max_length=15)
+#   idFornecedor = models.ForegnKey(Fornecedor,on_delete=models.CASCADE)
+#   nomeProduto=models.CharField(max_length= 30)
+#   valorUnit= models.DecimalField(max_digits=10,decimal_places=2)
+#   qntEstoque =models.IntegerField(default = 1)
+#   categoria = models.CharField(max_length=15)
+#   subCategoria=models.CharField(max_length=15)
+#   marcaProduto=models.CharField(max_length=15)
 
 class Fornecedor(models.Model):
     idFornecedor = models.AutoField(primary_key=True)
