1747c805a548a43d20bcb55a71b0ae74f72438b9
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-20 15:23:43 -0300

    Deletando Modelo Produtos

--- Produtos/models.py
+++ Produtos/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Produtos(models.Model):
-    idProduto = models.AutoField(primary_key=True)
-    nomeProduto=models.CharField(max_length= 30)
-    valorUnit= models.DecimalField(max_digits=10,decimal_places=2)
-    qntEstoque =models.IntegerField(default = 1)
-    categoria = models.CharField(max_length=15)
-    subCategoria=models.CharField(max_length=15)
-    marcaProduto=models.CharField(max_length=15)
+
+#class Produtos(models.Model):
+#   idProduto = models.AutoField(primary_key=True)
+#  nomeProduto=models.CharField(max_length= 30)
+# valorUnit= models.DecimalField(max_digits=10,decimal_places=2)
+#qntEstoque =models.IntegerField(default = 1)
+#categoria = models.CharField(max_length=15)
+#    subCategoria=models.CharField(max_length=15)
+#    marcaProduto=models.CharField(max_length=15)
