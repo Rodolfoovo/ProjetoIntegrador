1337385b2305f4bbf3a7d7e3c9e215e322ce7033
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-22 19:58:37 -0300

    criando as views da back do produto e do fornecedor

--- Produtos/views.py
+++ Produtos/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produtos
 from .forms import criaProdutoForm
+from django.http import HttpResponse
 # Create your views here.
 def produtos_view(request):
     produtos = Produtos.objects.all()
     produtoForm = criaProdutoForm()
     return render(request,"produtos.html", {"produtos": produtos, 'produtoForm':produtoForm})
 
+def criaProdutos_view(request):
+    if request.method == 'POST':
+        form = criaProdutoForm(request.POST)
+        if form.is_valid():
+            produtos = form.save(commit=False)
+            produtos.save()
+            return redirect(produtos_view)
+def editar_view(request, id):
+    produto =Produtos.objects.get(idProduto=id) 
+    return render(request, "updateProduto.html", {"produto": produto})
 
+def update_view(request,id):
+    if request.method == 'POST':
+        produtos = Produtos.objects.get(idProduto=id)
+        form = criaProdutoForm(request.POST,instance=produtos)
+        if form.is_valid():
+            form.save()
+            return redirect(produtos_view)
+    else:
+        # Se o método não for POST, redirecione para a página de origem ou trate conforme necessário
+        return HttpResponse('Método não permitido')
     
