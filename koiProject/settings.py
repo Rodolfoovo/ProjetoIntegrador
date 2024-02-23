bd3f0579fcbc5b9e02d01b0968d12f4218b448b3
Jessé Santana Veloso <jexvs890@gmail.com>
2024-02-23 00:19:17 -0300

    Retangulos de cadastro

--- koiProject/settings.py
+++ koiProject/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'login', 'static'),
+    os.path.join(BASE_DIR,'Login', 'static'),
 ]
 
 # Default primary key field type
