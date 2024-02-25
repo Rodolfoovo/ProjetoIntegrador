1f647ffe1bc9631d038a8611d9bbc01b6cea0244
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-02-25 09:23:09 -0300

    Importando valida_cep e valida_cpnj

--- Fornecedores/views.py
+++ Fornecedores/views.py
@@ -3,6 +3,7 @@ from .models import Fornecedor
 from .forms import FornecedorForm
 from django.shortcuts import redirect
 from django.http import HttpResponse
+import re
 # Create your views here.
 
 def fornecedor_view(request):
@@ -37,3 +38,38 @@ def deleteFornecedor_view(request, id):
     fornecedor = Fornecedor.objects.get(idFornecedor=id) 
     fornecedor.delete()
     return redirect(fornecedor_view)
+
+def valida_cnpj(cnpj):
+    #Função para validar o cnpj, será aplicada em trabalhos futuros
+    cnpj = ''.join(re.findall('\\d', str(cnpj)))
+
+    if (len(cnpj) != 14):
+        return False
+
+    inteiros = list(map(int, cnpj))
+    novo = inteiros[:12]
+
+    prod = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    while len(novo) < 14:
+        r = sum([x*y for (x, y) in zip(novo, prod)]) % 11
+        if r > 1:
+            f = 11 - r
+        else:
+            f = 0
+        novo.append(f)
+        prod.insert(0, 6)
+
+    if novo == inteiros:
+        return True
+
+    return False
+
+def valida_cep(cep):
+    # A função re.match tenta combinar a string fornecida com a expressão regular
+    # A expressão regular r'^\d{5}-\d{3}$' corresponde a uma string que começa (^) com 5 dígitos (\d{5}), seguida por um hífen (-), e termina ($) com 3 dígitos (\d{3})
+    # Se a string do CEP combinar com essa expressão regular, a função re.match retornará um objeto de correspondência, caso contrário, retornará None
+    # O operador 'is not None' verifica se o resultado da função re.match é diferente de None, ou seja, se houve uma correspondência
+    # Portanto, a função valida_cep retorna True se o CEP estiver no formato correto e False caso contrário
+    return re.match(r'^\d{5}-\d{3}$', cep) is not None
+
+
