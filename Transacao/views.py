5e39301aaa8c6297b912681da0ec0155a8ceb25a
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-06-04 15:25:06 -0300

    Criacao do metodo transacao_view

--- Transacao/views.py
+++ Transacao/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Transacao
 # Create your views here.
+
+def transacao_view(request):
+    transacoes = Transacao.objects.all()
+    return render(request,"transacao.html",{transacoes: "transacoes"})
+
+#def salvarTransacao_view(request):
