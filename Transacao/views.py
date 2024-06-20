f44619e53f2d6f76b7d5361504db07cddbf7cc3e
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-06-20 16:46:02 -0300

    Refatorando a views da Transacao

--- Transacao/views.py
+++ Transacao/views.py
@@ -1,52 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
-from django.shortcuts import redirect
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
-# Create your views here.
 
 def transacao_view(request):
     transacoes = Transacao.objects.all()
-    return render(request,"transacao.html",{transacoes: "transacoes"})
+    return render(request, "transacao.html", {"transacoes": transacoes})
 
 def salvarTransacao_view(request):
     if request.method == 'POST':
         vDataTransacao = request.POST.get("dataTransacao")
         vTipoTransacao = request.POST.get("tipoTransacao")
-        transacao = Transacao(
-            dataTransacao=vDataTransacao, 
-            tipoTransacao=vTipoTransacao
-        )
+        transacao = Transacao(dataTransacao=vDataTransacao, tipoTransacao=vTipoTransacao)
         try:
             transacao.full_clean()
         except ValidationError as e:
-            return HttpResponse(f"Erro de validacao do formulário: {e}")
-        transacao = Transacao.objects.create(
-            dataTransacao = vDataTransacao,
-            tipoTransacao=vTipoTransacao
-        )
+            return HttpResponse(f"Erro de validação do formulário: {e}")
+        transacao.save()
     return redirect(transacao_view)
 
-def editarTransacao_view(request,id):
+def editarTransacao_view(request, id):
     transacao = Transacao.objects.get(idTransacao=id)
-    return render(request, "updateTransacao.html",{"transacao":transacao})
+    return render(request, "updateTransacao.html", {"transacao": transacao})
 
-def updateTransacao_view(request,id):
+def updateTransacao_view(request, id):
     if request.method == 'POST':
-        transacao = Transacao.objects.get(id)
+        transacao = Transacao.objects.get(id=id)
         transacao.dataTransacao = request.POST.get("dataTransacao")
         transacao.tipoTransacao = request.POST.get("tipoTransacao")
         try:
             transacao.full_clean()
         except ValidationError as e:
-            return HttpResponse(f"Erro de validacao do formulário: {e}")
+            return HttpResponse(f"Erro de validação do formulário: {e}")
         transacao.save()
         return redirect(transacao_view)
 
-def deletarTransacao_view(request,id):
+def deletarTransacao_view(request, id):
     try:
-        transacao = Transacao.objects.get(id)
+        transacao = Transacao.objects.get(id=id)
         transacao.delete()
-        redirect(transacao_view)
+        return redirect(transacao_view)
     except Transacao.DoesNotExist:
-        HttpResponse("Ocorreu um erro ao deletar o objeto.")
+        return HttpResponse("Ocorreu um erro ao deletar o objeto.")
