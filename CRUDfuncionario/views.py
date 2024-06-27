4bcccf7c84132ca80efc7d78950b193a89dde2d2
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-06-27 14:15:38 -0300

    Adicionando ao funcionário e o fonrecedor o redirecionamento para  a tela de login

--- CRUDfuncionario/views.py
+++ CRUDfuncionario/views.py
@@ -16,69 +16,81 @@ def salvarFuncionario_view(request):
     if request.method == 'GET':
         return render(request, "funcionarios.html")
     elif request.method == 'POST':
+        if(verifica_login(request)):
         # Verifica se já existe um usuário com o mesmo username
-        novo_funcionario = Funcionario(
-                nivelDeAcesso=1,
-                username=request.POST.get("username"),
-                enderecoFuncionario=request.POST.get("enderecoFuncionario"),
-                CPF=request.POST.get("CPF"),
-                CEP=request.POST.get("CEP"),
-                telefone=request.POST.get("telefone"),
-                password=request.POST.get("password"),
-                funcao=request.POST.get("funcao"),
-                email=request.POST.get("email")
-            )
-        try:
-            novo_funcionario.full_clean()
-            usuarioAux = Funcionario.objects.get(username=request.POST.get("username"))
-            return redirect(Funcionarios)
-        except ValidationError as e:
-                return HttpResponse(f"Erro de validacao do formulário: {e}")
-        except Funcionario.DoesNotExist:
-            # Autentica o novo usuário
-            novo_funcionario =Funcionario.objects.create_user(
-                nivelDeAcesso=1,
-                username=request.POST.get("username"),
-                enderecoFuncionario=request.POST.get("enderecoFuncionario"),
-                CPF=request.POST.get("CPF"),
-                CEP=request.POST.get("CEP"),
-                telefone=request.POST.get("telefone"),
-                password=request.POST.get("password"),
-                funcao=request.POST.get("funcao"),
-                email=request.POST.get("email")
-            )
-            user = authenticate(request, username=request.POST.get("username"), password=request.POST.get("password"))
-            
-            if user:
-                funcionarios = Funcionario.objects.all()
+            novo_funcionario = Funcionario(
+                    nivelDeAcesso=1,
+                    username=request.POST.get("username"),
+                    enderecoFuncionario=request.POST.get("enderecoFuncionario"),
+                    CPF=request.POST.get("CPF"),
+                    CEP=request.POST.get("CEP"),
+                    telefone=request.POST.get("telefone"),
+                    password=request.POST.get("password"),
+                    funcao=request.POST.get("funcao"),
+                    email=request.POST.get("email")
+                )
+            try:
+                novo_funcionario.full_clean()
+                usuarioAux = Funcionario.objects.get(username=request.POST.get("username"))
                 return redirect(Funcionarios)
-            else:
-                return HttpResponse(user)
+            except ValidationError as e:
+                    return HttpResponse(f"Erro de validacao do formulário: {e}")
+            except Funcionario.DoesNotExist:
+                # Autentica o novo usuário
+                novo_funcionario =Funcionario.objects.create_user(
+                    nivelDeAcesso=1,
+                    username=request.POST.get("username"),
+                    enderecoFuncionario=request.POST.get("enderecoFuncionario"),
+                    CPF=request.POST.get("CPF"),
+                    CEP=request.POST.get("CEP"),
+                    telefone=request.POST.get("telefone"),
+                    password=request.POST.get("password"),
+                    funcao=request.POST.get("funcao"),
+                    email=request.POST.get("email")
+                )
+                user = authenticate(request, username=request.POST.get("username"), password=request.POST.get("password"))
+                
+                if user:
+                    funcionarios = Funcionario.objects.all()
+                    return redirect(Funcionarios)
+                else:
+                    return HttpResponse(user)
+        else:
+            redirect(login_view)
 def editar_view(request, id):
-    funcionario = Funcionario.objects.get(idFuncionario=id) 
-    return render(request, "update.html", {"funcionario": funcionario})
+    if(verifica_login(request)):
+        funcionario = Funcionario.objects.get(idFuncionario=id) 
+        return render(request, "update.html", {"funcionario": funcionario})
+    else:
+        return redirect(login_view)
 def update_view(request, id):
     if request.method == 'POST':
-        funcionario = Funcionario.objects.get(idFuncionario=id)
+        if(verifica_login(request)):
+            funcionario = Funcionario.objects.get(idFuncionario=id)
 
-        # Atualiza os campos do funcionário com base nos dados do formulário
-        funcionario.username = request.POST.get("username")
-        funcionario.enderecoFuncionario = request.POST.get("enderecoFuncionario")
-        funcionario.CPF = request.POST.get("CPF")
-        funcionario.CEP = request.POST.get("CEP")
-        funcionario.telefone = request.POST.get("telefone")
-        funcionario.email = request.POST.get("email")
-        funcionario.funcao = request.POST.get("funcao")
-        try:
-            funcionario.full_clean()
-        except ValidationError as e:
-            return HttpResponse(f"Erro de validacao do formulário: {e}")
-        funcionario.save()
-        return redirect(Funcionarios)
+            # Atualiza os campos do funcionário com base nos dados do formulário
+            funcionario.username = request.POST.get("username")
+            funcionario.enderecoFuncionario = request.POST.get("enderecoFuncionario")
+            funcionario.CPF = request.POST.get("CPF")
+            funcionario.CEP = request.POST.get("CEP")
+            funcionario.telefone = request.POST.get("telefone")
+            funcionario.email = request.POST.get("email")
+            funcionario.funcao = request.POST.get("funcao")
+            try:
+                funcionario.full_clean()
+            except ValidationError as e:
+                return HttpResponse(f"Erro de validacao do formulário: {e}")
+            funcionario.save()
+            return redirect(Funcionarios)
+        else:
+            return redirect(login_view)
     else:
         # Se o método não for POST, redirecione para a página de origem ou trate conforme necessário
         return HttpResponse('Método não permitido')
 def delete_view(request, id):
-    funcionario = Funcionario.objects.get(idFuncionario=id) 
-    funcionario.delete()
-    return redirect(Funcionarios)
+    if(verifica_login(request)):
+        funcionario = Funcionario.objects.get(idFuncionario=id) 
+        funcionario.delete()
+        return redirect(Funcionarios)
+    else:
+        return redirect(login_view)
--- Fornecedores/views.py
+++ Fornecedores/views.py
@@ -4,59 +4,74 @@ from .forms import FornecedorForm
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.core.exceptions import ValidationError
-from django.contrib.auth.decorators import login_required
+from login.views import verifica_login, login_view
 import re
 # Create your views here.
 
 def fornecedor_view(request):
-    fornecedores = Fornecedor.objects.all()
-    fornecedorForm = FornecedorForm()
-    return render(request,"fornecedor.html",{"fornecedores":fornecedores, 'fornecedorForm': fornecedorForm})
+    if(verifica_login(request)):
+        fornecedores = Fornecedor.objects.all()
+        fornecedorForm = FornecedorForm()
+        return render(request,"fornecedor.html",{"fornecedores":fornecedores, 'fornecedorForm': fornecedorForm})
+    else:
+        return redirect(login_view)
 
 def salvarFornecedor_view(request):
     if request.method == 'POST':
-#        form = FornecedorForm(request.POST)
-        fornecedor = Fornecedor(
-            nomeFornecedor=request.POST.get("nomeFornecedor"),
-            endereco=request.POST.get("endereco"),
-            telefone=request.POST.get("telefone"),
-            cep=request.POST.get("cep"),
-            cnpj=request.POST.get("cnpj"))
-        try:
-            fornecedor.full_clean()
-        except ValidationError as e:
-            return HttpResponse(f"Erro de validacao do formulário: {e}")
-        fornecedor = Fornecedor.objects.create(
-            nomeFornecedor=request.POST.get("nomeFornecedor"),
-            endereco=request.POST.get("endereco"),
-            telefone=request.POST.get("telefone"),
-            cep=request.POST.get("cep"),
-            cnpj=request.POST.get("cnpj")
-            )
-        fornecedor.save()
-        return redirect(fornecedor_view)
+        if(verifica_login(request)):
+    #        form = FornecedorForm(request.POST)
+            fornecedor = Fornecedor(
+                nomeFornecedor=request.POST.get("nomeFornecedor"),
+                endereco=request.POST.get("endereco"),
+                telefone=request.POST.get("telefone"),
+                cep=request.POST.get("cep"),
+                cnpj=request.POST.get("cnpj"))
+            try:
+                fornecedor.full_clean()
+            except ValidationError as e:
+                return HttpResponse(f"Erro de validacao do formulário: {e}")
+            fornecedor = Fornecedor.objects.create(
+                nomeFornecedor=request.POST.get("nomeFornecedor"),
+                endereco=request.POST.get("endereco"),
+                telefone=request.POST.get("telefone"),
+                cep=request.POST.get("cep"),
+                cnpj=request.POST.get("cnpj")
+                )
+            fornecedor.save()
+            return redirect(fornecedor_view)
+        else:
+            return redirect(login_view)
 def editarFornecedor_view(request, id):
-    fornecedor = Fornecedor.objects.get(idFornecedor=id) 
-    return render(request, "updateFornecedor.html", {"fornecedor": fornecedor})
+    if(verifica_login(request)):
+        fornecedor = Fornecedor.objects.get(idFornecedor=id) 
+        return render(request, "updateFornecedor.html", {"fornecedor": fornecedor})
+    else:
+        redirect(login_view)
 
 def updateFornecedor_view(request,id):
     if request.method == 'POST':
-        fornecedor = Fornecedor.objects.get(idFornecedor=id)
-        fornecedor.nomeFornecedor = request.POST.get("nomeFornecedor")
-        fornecedor.endereco = request.POST.get("endereco")
-        fornecedor.telefone = request.POST.get("telefone")
-        fornecedor.cep = request.POST.get("cep")
-        fornecedor.cnpj = request.POST.get("cnpj")
-        try:
-            fornecedor.full_clean()
-        except ValidationError as e:
-            return HttpResponse(f"Erro de validacao do formulário: {e}")
-        fornecedor.save()
-        return redirect(fornecedor_view)
+        if(verifica_login(request)):
+            fornecedor = Fornecedor.objects.get(idFornecedor=id)
+            fornecedor.nomeFornecedor = request.POST.get("nomeFornecedor")
+            fornecedor.endereco = request.POST.get("endereco")
+            fornecedor.telefone = request.POST.get("telefone")
+            fornecedor.cep = request.POST.get("cep")
+            fornecedor.cnpj = request.POST.get("cnpj")
+            try:
+                fornecedor.full_clean()
+            except ValidationError as e:
+                return HttpResponse(f"Erro de validacao do formulário: {e}")
+            fornecedor.save()
+            return redirect(fornecedor_view)
+        else:
+            return redirect(login_view)
     
 def deleteFornecedor_view(request, id):
-    fornecedor = Fornecedor.objects.get(idFornecedor=id) 
-    fornecedor.delete()
-    return redirect(fornecedor_view)
+    if(verifica_login(request)):
+        fornecedor = Fornecedor.objects.get(idFornecedor=id) 
+        fornecedor.delete()
+        return redirect(fornecedor_view)
+    else:
+        return redirect(login_view)
 
 
