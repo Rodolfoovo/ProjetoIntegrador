e57270802c1d81f8e87e287c5b9b98c29047e455
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-07-27 15:23:49 -0300

    testando os valores estaticos do dashboard, além de corrigir o bug relacionado a edição do funcionário

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from login.views import login_view, verifica_login
 from Produtos.models import Produtos
+import time
 def tela_inicial(request):
     if(verifica_login(request)):
         estoqueTotal = Produtos.calcular_estoque_total()
-#        valorEstoqueTotal = Produtos.calcular_valorEstoque_total()
-#        entradaProdutosMensal = Produtos.entrada_produtos_mensal()
-#        saidaProdutosMensal = Produtos.saida_produtos_mensal()
-        return render(request, 'telainicial.html',{"estoqueTotal": estoqueTotal})
+        valorEstoqueTotal = Produtos.calcular_valorEstoque_total()
+        entradaProdutosMensal = Produtos.entrada_produtos_mensal()
+        saidaProdutosMensal = Produtos.saida_produtos_mensal()
+        return render(request, 'telainicial.html',{"estoqueTotal": estoqueTotal,
+                                                    "valorEstoqueTotal": valorEstoqueTotal,
+                                                    "EntradaProdutosMensal": entradaProdutosMensal,
+                                                    "SaidaProdutosMensal":saidaProdutosMensal})
     else:
         return redirect(login_view)
